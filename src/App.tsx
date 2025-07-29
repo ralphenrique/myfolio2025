68d7c8a85594522dd16521b09431ed833cd7f2fa
ralphenrique <renrique_220000001478@uic.edu.ph>
2025-07-30 00:43:59 +0800

    removed experience

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ export default function App() {
         <Hero />
         <About />
         <Skills />
-        <Experience />
+        {/* <Experience /> */}
         <Interests />
         <Footer />
       </div>
